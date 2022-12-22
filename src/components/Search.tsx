60aedbda535ccf860dfb33f47b9a2a3e0b18bca1
Edgar Chavero <edieveg316@gmail.com>
2022-12-22 14:02:43 -0600

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback } from "react";
-import { TextField } from "@mui/material";
+import { TextField, Box } from "@mui/material";
 
 interface SearchProps {
   getQuery: (query: string) => void;
@@ -15,13 +15,21 @@ function Search({ getQuery }: SearchProps) {
 
   return (
     <section className="search">
-      <TextField
-        id="outlined-basic"
-        label="Search"
-        variant="outlined"
-        onChange={onChange}
-        size="small"
-      />
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          marginTop: "45px",
+        }}>
+        <TextField
+          id="outlined-basic"
+          label="Search"
+          variant="outlined"
+          onChange={onChange}
+          size="small"
+        />
+      </Box>
     </section>
   );
 }
